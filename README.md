3f64fe7292c2a336db1db81b61b283cf1f4a491b
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-08-30 20:23:05 +0200

    Oppjustert for natursystem

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Geojson-subset-kart-lastejobb
 
-Leser GeoJSON kartfil med alle typer og splitter denne opp i GeoJSON filer for hver underkatalog hvor typer som ikke hører med er filtrert bort.
+Leser GeoJSON lines kartfil med alle typer og splitter denne opp i GeoJSON filer for hver underkatalog hvor typer som ikke hører med er filtrert bort.
 
 Avhengig av kildekartet inneholder en property med navn `kode` på hver GeoJSON feature. Denne korreleres mot innhold i metadata.json fil som forventes å eksistere i underkatalogene.
 
--- geojson-subset-kart-lastejobb.js
+++ geojson-subset-kart-lastejobb.js
@@ -2,34 +2,55 @@
 const fs = require("fs");
 const path = require("path");
 const { io, log } = require("lastejobb");
-const pjson = require("./package");
+//const pjson = require("./package");
+const readline = require('readline');
 
-log.info(pjson.name + " v" + pjson.version + ": " + pjson.description);
+//log.info(pjson.name + " v" + pjson.version + ": " + pjson.description);
 
 let kildekart = "polygon_med_undertyper.4326.geojson";
+let rootPath = './'
 if (process.argv.length > 2) kildekart = process.argv[2];
+if (process.argv.length > 3) rootPath = process.argv[3];
+
 log.info("Kildekart: " + kildekart);
 if (!fs.existsSync(kildekart))
   return log.error("Mangler kildefil " + kildekart);
 let levels = 99;
-if (process.argv.length > 3) levels = parseInt(process.argv[3]);
+if (process.argv.length > 3) levels = parseInt(process.argv[3]) || 99;
 log.info("Nesting levels: " + levels);
 
-const subkart = io.readJson(kildekart);
-const features = subkart.features;
+lagSubkart(path.resolve(rootPath));
 
-walkSync("./", levels);
+function makeMap(meta, targetDir) {
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(kildekart)
+  });
 
-function walkSync(dir, level) {
-  log.info(dir);
-  lagSubkart(dir);
-  if (level === 0) return;
-  let files = fs.readdirSync(dir);
-  files.forEach(function(file) {
-    const sub = path.join(dir, file);
-    if (fs.statSync(sub).isDirectory()) {
-      filelist = walkSync(sub + "/", level - 1);
-    }
+  var subkart = {
+    "name": "polygons",
+    "title": meta.tittel.nb,
+    "created": new Date(),
+    "type": "FeatureCollection",
+    features: []
+  }
+  readInterface.on('line', function (line) {
+    if (line.length <= 0) return
+    const feature = JSON.parse(line)
+    const props = feature.properties
+    let koder = props.kode || props.koder;
+    if (!Array.isArray(koder)) koder = [koder];
+    let kode = finnKode(meta, koder);
+    if (!kode) return;
+
+    props.kode = kode.split("-").pop();
+    subkart.features.push(feature);
+  });
+
+  readInterface.on('close', function () {
+    if (subkart.features.length <= 0) return log.warn("Tomt kart for " + targetDir);
+    const kartpath = path.join(targetDir, "polygon_no_dissolve.4326.geojson");
+    log.info("Skriver " + kartpath);
+    fs.writeFileSync(kartpath, JSON.stringify(subkart));
   });
 }
 
@@ -38,35 +59,18 @@ function lagSubkart(dir) {
   if (!fs.existsSync(metapath))
     return log.warn("Mangler metadata.json i " + metapath);
   const meta = io.readJson(metapath);
-  log.info("Leser " + metapath);
-  subkart.features = [];
-  features.forEach(f => {
-    let koder = f.properties.kode || f.properties.koder;
-    if (!Array.isArray(koder)) koder = [koder];
-    let kode = finnKode(meta, koder);
-    if (!kode) return;
-
-    const f2 = JSON.parse(JSON.stringify(f));
-    const props = f2.properties;
-    props.kode = kode.split("-").pop();
-    subkart.features.push(f2);
-  });
-  if (subkart.features.length <= 0) return log.warn("Tomt kart for " + dir);
-
-  const kartpath = path.join(dir + "polygon_no_dissolve.4326.geojson");
-  log.info("Skriver " + kartpath);
-  fs.writeFileSync(kartpath, JSON.stringify(subkart));
+  makeMap(meta, dir)
 }
 
 function finnKode(meta, kartkoder) {
   if (!kartkoder)
     throw new Error("Required property code is missing in " + kildekart);
   for (var kode of kartkoder) {
-    for (var barn of meta.barn) {
-      if (kode.indexOf(barn.kode) === 0) return barn.kode;
-    }
-    if (meta.barn.length <= 0 && kode.indexOf(meta.kode) === 0)
+    for (var barn of meta.barn)
+      if (kode.indexOf(barn.kode) === 0)
+        return barn.kode;
+    if (meta.barn.length <= 0 && kode.indexOf(meta.kode) === 0) {
       return meta.kode;
+    }
   }
-  return null;
 }
